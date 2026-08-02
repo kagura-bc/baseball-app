831ced12046a29e6ffc300aa1669f700fd3928bd
baseball-user <k.nakao111@gmail.com>
2026-08-02 17:38:25 +0900

    選手データの管理

--- config/settings.py
+++ config/settings.py
@@ -1,16 +1,11 @@
 # config/settings.py
 import streamlit as st
+
 # チーム名
 MY_TEAM = "KAGURA"
 
 # スプレッドシート情報 (Secretsから取得)
 SPREADSHEET_URL = st.secrets["SPREADSHEET_URL"]
-
-# 選手データ (Secretsから取得)
-PLAYER_NUMBERS = dict(st.secrets["PLAYER_NUMBERS"])
-
-# リスト生成
-ALL_PLAYERS = list(PLAYER_NUMBERS.keys())
 
 # ポジションリスト
 ALL_POSITIONS = ["", "DH", "投", "捕", "一", "二", "三", "遊", "左", "中", "右"] 
